9964d7e9f8c59736c52f35586aca78bf8c558957
Eyck Jentzsch <eyck@minres.com>
2024-04-25 14:36:28 +0200

    fixes tcc package build issues on newer ubuntu

--- TCC/conanfile.py
+++ TCC/conanfile.py
@@ -4,8 +4,8 @@ import shutil
 
 class TccConan(ConanFile):
     name = "tcc"
-    revision = "62c30a4a"
-    version = "0.9.27+%s" % revision if revision else "0.9.27"
+    revision = "1645616843"
+    version = "0.9.27"
     license = "GNU Lesser General Public License"
     author = "Eyck Jentzsch <eyck@minres.com>"
     url = "https://github.com/Minres/conan-recipes/tree/master/TCC"
@@ -13,25 +13,18 @@ class TccConan(ConanFile):
     topics = ("compiler", "c")
     settings = "os", "compiler", "build_type", "arch"
     default_options = {}
-    source_tar = "http://download.savannah.gnu.org/releases/tinycc/tcc-%s.tar.bz2" % version
     git_repo = "git://repo.or.cz/tinycc.git"
     #generators = "cmake"
     sub_folder = "tcc-%s" % version
     exports_sources = "tcc-%s/*" % version
 
     def source(self):
-        if self.revision:
-            self.output.info("Cloning from %s" % self.git_repo)
-            git = tools.Git(folder=self.sub_folder)
-            if os.path.exists(self.sub_folder):
-                shutil.rmtree(self.sub_folder) 
-            git.clone(self.git_repo)
-            git.checkout(self.revision)
-        else:
-            self.output.info("Downloading %s" % self.source_tar)
-            tools.download(self.source_tar, "tcc.tar.bz2")
-            tools.unzip("tcc.tar.bz2")
-            os.remove("tcc.tar.bz2")
+        self.output.info("Cloning from %s" % self.git_repo)
+        git = tools.Git(folder=self.sub_folder)
+        if os.path.exists(self.sub_folder):
+            shutil.rmtree(self.sub_folder) 
+        git.clone(self.git_repo)
+        git.checkout(self.revision)
 
     def configure(self):
         del self.settings.compiler.libcxx
